f7a05efa1469b51490a65401f568e97703b80075
Ali Khatib <alikhatib2004@gmail.com>
2024-05-13 00:04:45 -0400

    New Project from Xcode

---
+++ Slate/SlateApp.swift
@@ -0,0 +1,32 @@
+//
+//  SlateApp.swift
+//  Slate
+//
+//  Created by Ali Khatib on 5/13/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SlateApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
